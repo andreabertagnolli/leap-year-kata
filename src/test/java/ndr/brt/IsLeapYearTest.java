f1d0c290adc5117390ac1c7e9d92c3f047f52616
Andrea Bertagnolli <andrea.bertagnolli@gmail.com>
2018-02-26 12:51:32 +0100

    Test refactor

--- src/test/java/ndr/brt/IsLeapYearTest.java
+++ src/test/java/ndr/brt/IsLeapYearTest.java
@@ -6,17 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class IsLeapYearTest {
+
+    private IsLeapYear isLeapYear = new IsLeapYear();
+
     @Test
     void typical_common_year_not_leap_year() {
-        IsLeapYear isLeapYear = new IsLeapYear();
-
         assertFalse(isLeapYear.test(2001));
     }
 
     @Test
     void typical_leap_year() {
-        IsLeapYear isLeapYear = new IsLeapYear();
-
         assertTrue(isLeapYear.test(1996));
     }
+
 }
